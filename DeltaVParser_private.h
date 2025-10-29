b2c77b718aeb91b856b1dd5db11df34e774b9095
claudiolop <claudiolop@gmail.com>
2025-10-28 19:32:00 -0500

    Improved Table and Type Config.

--- DeltaVParser_private.h
+++ DeltaVParser_private.h
@@ -5,19 +5,19 @@
 #define DELTAVPARSER_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"1.0.1.0"
+#define VER_STRING	"1.0.2.0"
 #define VER_MAJOR	1
 #define VER_MINOR	0
-#define VER_RELEASE	1
+#define VER_RELEASE	2
 #define VER_BUILD	0
 #define COMPANY_NAME	"Claudio Lopez"
-#define FILE_VERSION	"1.0.1.0"
+#define FILE_VERSION	"1.0.2.0"
 #define FILE_DESCRIPTION	"DeltaVParser"
 #define INTERNAL_NAME	""
 #define LEGAL_COPYRIGHT	""
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	""
 #define PRODUCT_NAME	"DeltaVParser"
-#define PRODUCT_VERSION	"1.0.1.0"
+#define PRODUCT_VERSION	"1.0.2.0"
 
 #endif /*DELTAVPARSER_PRIVATE_H*/
